b62d87915c52cf8df461c746bc8979cc664516f5
Maxime Kofi <kofi.maxime@gmail.com>
2026-04-16 14:48:17 +0200

    Dockerfile adjutement

--- eventhub-app.sh
+++ eventhub-app.sh
@@ -1,7 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo "********************************"
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 5000"
+
+docker cp $(hostname):/var/jenkins_home/workspace/EventHubJob/nginx/default.conf /tmp/nginx-default.conf
+
+export NGINX_CONF_PATH=/tmp/nginx-default.conf
 docker compose up -d --build
+
 echo "********************************"
 date
