d6dbc0ac1109779ef38b90152344edc46340fdfb
Maxime Kofi <kofi.maxime@gmail.com>
2026-04-16 15:23:59 +0200

    Dockerfile adjutement

--- eventhub-app.sh
+++ eventhub-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "********************************"
-echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 5000"
+echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 80"
 docker compose up -d --build
 echo "********************************"
 date
